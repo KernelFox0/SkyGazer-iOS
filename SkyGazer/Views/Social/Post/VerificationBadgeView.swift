99793b9c26ca9767f96ad0cf881be29da5e122cc
KernelFox0 <65031582+KernelFox0@users.noreply.github.com>
2025-12-14 16:23:48 +0100

    Add info sheet for verifications

--- SkyGazer/Views/Social/Post/VerificationBadgeView.swift
+++ SkyGazer/Views/Social/Post/VerificationBadgeView.swift
@@ -13,6 +13,8 @@ struct VerificationBadgeView: View {
 	
 	let verified: Verification?
 	
+	@State private var presentInformationSheet: Bool = false
+	
 	@Environment(PreferenceManager.self) private var preferenceManager
 	
 	init(verified: Verification?) {
@@ -31,7 +33,48 @@ struct VerificationBadgeView: View {
 			}
 			.foregroundStyle(preferenceManager.accentColor)
 			.font(.subheadline)
+			.onTapGesture {
+				presentInformationSheet = true
+			}
 			.padding(-5)
+			.sheet(isPresented: $presentInformationSheet) {
+				verificationInfoSheetView(verified)
+			}
+		}
+	}
+	
+	@ViewBuilder
+	private func verificationInfoSheetView(_ verified: Verification) -> some View {
+		if let isTrustedVerification =	verified.trustedVerifiedStatus == .valid
+										? true
+										: (verified.verifiedStatus == .valid
+										   ? false
+										   : nil
+										)
+		{
+			VStack {
+				Image(systemName: isTrustedVerification ? "checkmark.seal.fill" : "checkmark.circle.fill")
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+					.frame(width: 50, height: 50)
+					.foregroundStyle(preferenceManager.accentColor)
+					.padding(.top, 10)
+					.padding(.bottom, 5)
+				Text(isTrustedVerification ? "Trusted Verifier" : "Verified")
+					.font(.title2)
+					.fontWeight(.semibold)
+					.padding(.bottom, 8)
+				Text(isTrustedVerification
+					 ? "These accounts can verify others. Trusted Verifiers are selected by Bluesky."
+					 : "This account has been verified by trusted sources."
+				)
+				.lineLimit(nil)
+				.multilineTextAlignment(.center)
+				Spacer(minLength: 0)
+			}
+			.frame(maxWidth: .infinity, alignment: .center)
+			.presentationDetents([.height(isTrustedVerification ? 200 : 170), .medium])
+			.padding()
 		}
 	}
 }
