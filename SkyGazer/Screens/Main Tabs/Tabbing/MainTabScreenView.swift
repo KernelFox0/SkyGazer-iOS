46fe7533a5069e6d1e1c19af0da3aab31f370c41
KernelFox0 <65031582+KernelFox0@users.noreply.github.com>
2025-12-14 19:32:51 +0100

    Add post creation button

--- SkyGazer/Screens/Main Tabs/Tabbing/MainTabScreenView.swift
+++ SkyGazer/Screens/Main Tabs/Tabbing/MainTabScreenView.swift
@@ -8,23 +8,29 @@
 import SwiftUI
 
 struct MainTabScreenView: View {
+	@State private var currentTabSelection: String = "home"
+	@State private var presentPostCreatorScreen: Bool = false
+	@State private var selectedDetent: PresentationDetent = .large
+	
+	@Environment(PreferenceManager.self) private var preferenceManager
+	
 	@FocusState var searchFocused
 	
 	var body: some View {
-		TabView {
-			Tab("Home", systemImage: "house") {
+		TabView(selection: $currentTabSelection) {
+			Tab("Home", systemImage: "house", value: "home") {
 				HomeScreenView()
 			}
-			Tab("Chats", systemImage: "bubble.left.and.text.bubble.right") {
+			Tab("Chats", systemImage: "bubble.left.and.text.bubble.right", value: "chat") {
 				Text("Chats tab")
 			}
-			Tab("Notifications", systemImage: "bell") {
+			Tab("Notifications", systemImage: "bell", value: "notif") {
 				Text("Notifications tab")
 			}
-			Tab("Profile", systemImage: "person") {
+			Tab("Profile", systemImage: "person", value: "user") {
 				UserScreenView(userDid: UserManager.shared.loggedInDID)
 			}
-			Tab("Search", systemImage: "magnifyingglass", role: .search) {
+			Tab("Search", systemImage: "magnifyingglass", value: "search", role: .search) {
 				NavigationStack {
 					Text("Search tab")
 						.searchable(text: .constant(""), placement: .navigationBarDrawer, prompt: Text("Search for people, tags and posts"))
@@ -32,7 +38,37 @@ struct MainTabScreenView: View {
 				}
 			}
 		}
+		.safeAreaInset(edge: .bottom, alignment: .trailing) {
+			createPostButton
+		}
+		.sheet(isPresented: $presentPostCreatorScreen) {
+			PostCreationScreenView()
+				.presentationDetents([.large, .medium], selection: $selectedDetent)
+		}
     }
+	
+	private var createPostButton: some View {
+		Image(systemName: currentTabSelection == "chat" ? "plus.bubble" : "plus")
+			.resizable()
+			.aspectRatio(contentMode: .fit)
+			.frame(width: 23, height: 23)
+			.foregroundStyle(.white)
+			.padding(17)
+			.glassEffect(.regular.tint(preferenceManager.accentColor).interactive(), in: Circle())
+			.contentShape(Circle())
+			.padding(10)
+			.padding(.bottom, 55)
+			.onTapGesture {
+				HapticsManager.impact(style: .medium)
+				
+				if currentTabSelection != "chat" {
+					selectedDetent = .large
+					presentPostCreatorScreen = true
+				} else {
+					
+				}
+			}
+	}
 }
 
 #Preview {
---
+++ SkyGazer/Screens/Post Creation/PostCreationScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  PostCreationScreenView.swift
+//  SkyGazer
+//
+//  Created by Kernel on 2025. 12. 14..
+//
+
+import SwiftUI
+
+struct PostCreationScreenView: View {
+    var body: some View {
+		Text("Hello, World!")
+    }
+}
+
+#Preview {
+    PostCreationScreenView()
+}
