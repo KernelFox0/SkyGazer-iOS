6fe4e7672482630120225e7d007624be722441e1
KernelFox0 <65031582+KernelFox0@users.noreply.github.com>
2025-12-15 10:47:14 +0100

    Add getting UI names to threadgate

--- SkyGazer/Models/ThreadGate.swift
+++ SkyGazer/Models/ThreadGate.swift
@@ -25,4 +25,16 @@ nonisolated struct ThreadGate {
 	var rules: [AllowRepliesRuleset] = []
 	
 	var userCanReply: Bool? = nil //This will store the value if the viewer can reply or not
+	
+	func uiName() -> String {
+		if allowQuotes && allowReplies && rules.isEmpty { return String(localized: "Everyone can interact") }
+		else if !allowReplies && !allowQuotes { return String(localized: "No one can interact") }
+		else { return String(localized: "Interaction limited") }
+	}
+	
+	func uiSystemImage() -> String {
+		if allowQuotes && allowReplies && rules.isEmpty { return "globe.europe.africa" }
+		else if !allowReplies && !allowQuotes { return "person.2" }
+		else { return "person.slash" }
+	}
 }
