848dcdb3686283d566dc1f0485de8bc15f747311
KernelFox0 <65031582+KernelFox0@users.noreply.github.com>
2026-06-21 20:28:42 +0200

    Remove a no longer needed temporary fix

--- SkyGazer/Extensions/ATProtoKit+TempFixes.swift
+++ SkyGazer/Extensions/ATProtoKit+TempFixes.swift
@@ -12,59 +12,6 @@ import FoundationNetworking
 import ATProtoKit
 
 extension ATProtoBluesky {
-	
-	/// Creates a follow record to follow a specific user account.
-	///
-	/// - Note: A delay of 5 milliseconds is added after the follow record is created; any shorter delay
-	/// may cause the next request to fail.
-	///
-	/// - Parameters:
-	///   - actorDID: The decentralized identifier (DID) for the user account to follow.
-	///   - createdAt: The date and time the follow record is created. Defaults to `Date()`.
-	///   - via: A strong reference to the user account that recommended the followed account. Optional.
-	///   - recordKey: The record key of the collection. Optional. Defaults to `nil`.
-	///   - shouldValidate: Indicates whether the record should be validated. Optional.
-	///   Defaults to `true`.
-	///   - swapCommit: Swaps out an operation based on the CID. Optional. Defaults to `nil`.
-	/// - Returns: A
-	/// ``ComAtprotoLexicon/Repository/StrongReference``
-	/// structure which represents the record that was successfully created.
-	public func createFollowRecord(
-		actorDID: String,
-		createdAt: Date = Date(),
-		via: ComAtprotoLexicon.Repository.StrongReference? = nil,
-		recordKey: String? = nil,
-		shouldValidate: Bool? = true,
-		swapCommit: String? = nil
-	) async throws -> ComAtprotoLexicon.Repository.StrongReference {
-		guard let session = try await UserManager.shared.ATProto?.getUserSession() else {
-			throw ATRequestPrepareError.missingActiveSession
-		}
-		
-		let followRecord = AppBskyLexicon.Graph.FollowRecord(
-			subjectDID: actorDID,
-			createdAt: createdAt,
-			via: via
-		)
-		
-		do {
-			guard let record = try await UserManager.shared.ATProto?.createRecord(
-				repositoryDID: session.sessionDID,
-				collection: "app.bsky.graph.follow",
-				recordKey: recordKey,
-				shouldValidate: shouldValidate,
-				record: UnknownType.record(followRecord),
-				swapCommit: swapCommit
-			) else { throw ATRequestPrepareError.missingActiveSession }
-			
-			try await Task.sleep(nanoseconds: 500_000_000)
-			
-			return record
-		} catch {
-			throw error
-		}
-	}
-	
 	/// A convenience method to create a threadgate record to the user account in Bluesky.
 	///
 	/// This can be used instead of creating your own method if you wish not to do so.
